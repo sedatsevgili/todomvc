64db45774c5f1bd1dcdac1985867f8e47f5c8430
sedatsevgili <sedatsevgili@gmail.com>
2014-04-04 06:24:35 +0300

    displayed incomplete todo count dynamically

--- web/js/controllers/todos_controllers.js
+++ web/js/controllers/todos_controllers.js
@@ -30,5 +30,12 @@ Todos.TodosController = Ember.ArrayController.extend({
 				return value;
 			}
 		}.property('model.isCompleted')
-	}
+	},
+	remaining: function() {
+		return this.filterBy('isCompleted', false).get('length');
+	}.property('@each.isCompleted'),
+	inflection: function() {
+		var remaining = this.get('remaining');
+		return remaining === 1 ? 'item' : 'items';
+	}.property('remaining')
 });	
