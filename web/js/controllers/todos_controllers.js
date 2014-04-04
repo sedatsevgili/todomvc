e7ae6f5d517f2418e3c69c8f9edd6d22c0da8dd5
sedatsevgili <sedatsevgili@gmail.com>
2014-04-04 06:19:53 +0300

    isComplete action method included

--- web/js/controllers/todos_controllers.js
+++ web/js/controllers/todos_controllers.js
@@ -16,6 +16,19 @@ Todos.TodosController = Ember.ArrayController.extend({
 			
 			// Save the model
 			todo.save();
-		}
+		},
+		isCompleted: function(key, value) {
+			var model = this.get('model');
+			
+			if(value == undefined) {
+				// property being used as a getter
+				return model.get('isCompleted');
+			} else {
+				// property being used as a setter
+				model.set('isCompleted', value);
+				model.save();
+				return value;
+			}
+		}.property('model.isCompleted')
 	}
 });	
