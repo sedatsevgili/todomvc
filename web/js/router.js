f7d0b3e7d1572591e4517c570cb53d2a9ed499d5
Sedat <sedat@ideasoft.com.tr>
2014-04-04 10:56:49 +0300

    active filter route included

--- web/js/router.js
+++ web/js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
 	// additional child routes    
+	this.route('active');
   });
 });
 
@@ -14,4 +15,15 @@ Todos.TodosIndexRoute = Ember.Route.extend({
   model: function () {
     return this.modelFor('todos');
   }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
+	}
 });
